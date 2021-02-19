08ec17fcf559185d94f8e0901e3efa688fd6e3a8
8Bts <mcmizze@yahoo.com>
2021-02-19 14:22:13 +0400

    Add todo.js module with ToDo factory

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
---
+++ src/todo.js
@@ -0,0 +1,9 @@
+/* eslint-disable linebreak-style */
+const ToDo = (title, description, dueDate, priority) => {
+  
+  return {
+    title, description, dueDate, priority, completed: false,
+  };
+};
+
+export default ToDo;
